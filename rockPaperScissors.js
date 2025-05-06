36ba7b4e20c1145d6e752cac0d7f70e70144f10d
MorrisKehbel <morris.kehbel@gmail.com>
2025-05-06 18:21:09 +0200

    more tidy

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -5,15 +5,16 @@ const items = ["Rock", "Paper", "Scissors"];
 const randomChoice = items[Math.floor(Math.random() * items.length)];
 
 const getResult = () => {
-  if (args == randomChoice) return "draw";
-  else if (args == "Rock" && randomChoice == "Scissors") return "won";
-  else if (args == "Rock" && randomChoice == "Paper") return "lost";
-  else if (args == "Paper" && randomChoice == "Rock") return "won";
-  else if (args == "Paper" && randomChoice == "Scissors") return "lost";
-  else if (args == "Scissors" && randomChoice == "Rock") return "lost";
-  else if (args == "Scissors" && randomChoice == "Paper") return "won";
+  if (args == randomChoice) return "Its a draw";
+  else if (
+    (args == "Rock" && randomChoice == "Paper") ||
+    (args == "Scissors" && randomChoice == "Rock") ||
+    (args == "Paper" && randomChoice == "Scissors")
+  ) {
+    return "You lost";
+  } else return "You won";
 };
 
 console.log(
-  `You chose ${args}. Computer chose ${randomChoice}. You ${getResult()}.`
+  `You chose ${args}. Computer chose ${randomChoice}. ${getResult()}.`
 );
